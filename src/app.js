36f354d0fee626d0af0986e258eb5837875c9cab
mxdlzg <mxdlzg@163.com>
2019-04-10 20:18:47 +0800

    添加redux-persisit后禁用

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 // import { persistStore, persistReducer } from 'redux-persist'
 // import storage from 'redux-persist/lib/storage'
-//
+// import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
 //
 // const persistConfig = {
 //     timeout: 1000,  // you can define your time. But is required.
 //     key: 'root',
 //     storage,
-//     whitelist: ['xxx'],
+//     //whitelist: ['xxx'],
+//     stateReconciler:autoMergeLevel2
 // };
 // const persistEnhancer = () => createStore => (reducer, initialState, enhancer) => {
 //     const store = createStore(persistReducer(persistConfig, reducer), initialState, enhancer);
