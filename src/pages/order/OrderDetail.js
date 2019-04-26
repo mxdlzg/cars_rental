f6d5efa8ee92383b2b01a029577c79b8d912de22
mxdlzg <mxdlzg@163.com>
2019-04-26 22:21:20 +0800

    detail页面布局调整完毕

--- src/pages/order/OrderDetail.js
+++ src/pages/order/OrderDetail.js
@@ -2,7 +2,7 @@ import React, {Component, Fragment} from 'react'
 import styles from './OrderDetail.less'
 import classNames from 'classnames';
 import DescriptionList from '@/components/DescriptionList';
-import PageHeader from 'ant-design-pro/lib/PageHeader';
+import PageHeader from '@/components/PageHeader';
 import {
     Button,
     Menu,
@@ -183,9 +183,8 @@ class OrderDetail extends Component {
         const {stepDirection} = this.state;
 
         return (
-            <div className={styles.main}>
+            <div>
                 <PageHeader
-                    className={styles.pageHeader}
                     title="单号：234231029431"
                     logo={
                         <img alt="" src="https://gw.alipayobjects.com/zos/rmsportal/nxkuOJlFJuAUhzlMTCEe.png"/>
@@ -196,79 +195,57 @@ class OrderDetail extends Component {
                     tabList={tabList}
                 >
                 </PageHeader>
-                <Card title="流程进度" style={{marginBottom: 24}} bordered={false}>
-                    <Steps direction={stepDirection} progressDot={customDot} current={1}>
-                        <Step title="创建项目" description={desc1}/>
-                        <Step title="部门初审" description={desc2}/>
-                        <Step title="财务复核"/>
-                        <Step title="完成"/>
-                    </Steps>
-                </Card>
-                <Card title="用户信息" style={{marginBottom: 24}} bordered={false}>
-                    <DescriptionList style={{marginBottom: 24}}>
-                        <Description term="用户姓名">付小小</Description>
-                        <Description term="会员卡号">32943898021309809423</Description>
-                        <Description term="身份证">3321944288191034921</Description>
-                        <Description term="联系方式">18112345678</Description>
-                        <Description term="联系地址">
-                            曲丽丽 18100000000 浙江省杭州市西湖区黄姑山路工专路交叉路口
-                        </Description>
-                    </DescriptionList>
-                    <DescriptionList style={{marginBottom: 24}} title="信息组">
-                        <Description term="某某数据">725</Description>
-                        <Description term="该数据更新时间">2017-08-08</Description>
-                        <Description>&nbsp;</Description>
-                        <Description
-                            term={
-                                <span>
-                  某某数据
-                  <Tooltip title="数据说明">
-                    <Icon
-                        style={{color: 'rgba(0, 0, 0, 0.43)', marginLeft: 4}}
-                        type="info-circle-o"
-                    />
-                  </Tooltip>
-                </span>
-                            }
-                        >
-                            725
-                        </Description>
-                        <Description term="该数据更新时间">2017-08-08</Description>
-                    </DescriptionList>
-                    <h4 style={{marginBottom: 16}}>信息组</h4>
-                    <Card type="inner" title="多层级信息组">
-                        <DescriptionList size="small" style={{marginBottom: 16}} title="组名称">
-                            <Description term="负责人">林东东</Description>
-                            <Description term="角色码">1234567</Description>
-                            <Description term="所属部门">XX公司 - YY部</Description>
-                            <Description term="过期时间">2017-08-08</Description>
-                            <Description term="描述">
-                                这段描述很长很长很长很长很长很长很长很长很长很长很长很长很长很长...
-                            </Description>
-                        </DescriptionList>
-                        <Divider style={{margin: '16px 0'}}/>
-                        <DescriptionList size="small" style={{marginBottom: 16}} title="组名称" col="1">
-                            <Description term="学名">
-                                Citrullus lanatus (Thunb.) Matsum. et
-                                Nakai一年生蔓生藤本；茎、枝粗壮，具明显的棱。卷须较粗..
-                            </Description>
-                        </DescriptionList>
-                        <Divider style={{margin: '16px 0'}}/>
-                        <DescriptionList size="small" title="组名称">
-                            <Description term="负责人">付小小</Description>
-                            <Description term="角色码">1234568</Description>
-                        </DescriptionList>
+                <div className={styles.main}>
+                    <Card title="流程进度" style={{marginBottom: 24}} bordered={false}>
+                        <Steps direction={stepDirection} progressDot={customDot} current={1}>
+                            <Step title="创建项目" description={desc1}/>
+                            <Step title="订单提交" description={desc2}/>
+                            <Step title="订单支付"/>
+                            <Step title="预订完成"/>
+                        </Steps>
                     </Card>
-                </Card>
-                <Card title="用户近半年来电记录" style={{marginBottom: 24}} bordered={false}>
-                    <div className={styles.noData}>
-                        <Icon type="frown-o"/>
-                        暂无数据
-                    </div>
-                </Card>
+                    <Card title="用户信息" style={{marginBottom: 24}} bordered={false}>
+                        <DescriptionList style={{marginBottom: 24}}>
+                            <Description term="用户姓名">付小小</Description>
+                            <Description term="会员卡号">32943898021309809423</Description>
+                            <Description term="身份证">332194428****034921</Description>
+                            <Description term="联系方式">18112***678</Description>
+                            <Description term="联系地址">
+                                曲丽丽 18100000000 浙江省杭州市西湖区黄姑山路工专路交叉路口
+                            </Description>
+                        </DescriptionList>
+                        <Card type="inner" title="多层级信息组">
+                            <DescriptionList size="small" style={{marginBottom: 16}} title="组名称">
+                                <Description term="负责人">林东东</Description>
+                                <Description term="角色码">1234567</Description>
+                                <Description term="所属部门">XX公司 - YY部</Description>
+                                <Description term="过期时间">2017-08-08</Description>
+                                <Description term="描述">
+                                    这段描述很长很长很长很长很长很长很长很长很长很长很长很长很长很长...
+                                </Description>
+                            </DescriptionList>
+                            <Divider style={{margin: '16px 0'}}/>
+                            <DescriptionList size="small" style={{marginBottom: 16}} title="组名称" col="1">
+                                <Description term="学名">
+                                    Citrullus lanatus (Thunb.) Matsum. et
+                                    Nakai一年生蔓生藤本；茎、枝粗壮，具明显的棱。卷须较粗..
+                                </Description>
+                            </DescriptionList>
+                            <Divider style={{margin: '16px 0'}}/>
+                            <DescriptionList size="small" title="组名称">
+                                <Description term="负责人">付小小</Description>
+                                <Description term="角色码">1234568</Description>
+                            </DescriptionList>
+                        </Card>
+                    </Card>
+                    <Card title="用户近半年来电记录" style={{marginBottom: 24}} bordered={false}>
+                        <div className={styles.noData}>
+                            <Icon type="frown-o"/>
+                            暂无数据
+                        </div>
+                    </Card>
+                </div>
             </div>
-
-
         )
     }
 }
